37bb0bc5753af9cb06960ed52c364b3fb1c52819
iksanh <iksanhariji@gmail.com>
2025-12-01 11:37:05 +0800

    fixed layout many to many field

--- apps/pages/forms.py
+++ apps/pages/forms.py
@@ -2,6 +2,9 @@ from dataclasses import field
 from django import forms
 from django.forms import inlineformset_factory, widgets
 from .models import Permohonan, Layanan, BerkasItem
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit, Row, Column
+from crispy_forms.bootstrap import InlineCheckboxes
 
 class PermohonanForm(forms.ModelForm):
     class Meta:
@@ -23,5 +26,23 @@ class LayananForm(forms.ModelForm):
 
 class BerkasItemForm(forms.ModelForm):
     class Meta:
-         model = BerkasItem
-         fields = ['nama']
+        model = BerkasItem
+        fields = ['nama', 'layanan']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # crispy form helper
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+
+        self.helper.layout = Layout(
+            Row(
+                Column('nama', css_class="mb-3"),
+            ),
+
+            # Checkbox layanan agar horizontal/rapi
+            InlineCheckboxes('layanan'),
+
+            Submit('submit', 'Simpan', css_class="btn btn-primary"),
+        )
